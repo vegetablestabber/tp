ac65ccee3dcb09db43d2f0eaa9a3a5fc7235245e
Harii <souper-maario-gaalaxi-wahoo@protonmail.com>
2025-03-20 17:07:24 +0800

    Refactored switch case formatting to follow checkstyle

--- src/main/java/modmate/mod/attribute/Semester.java
+++ src/main/java/modmate/mod/attribute/Semester.java
@@ -23,12 +23,12 @@ public enum Semester {
 
     public static Semester fromInt(int value) throws IllegalArgumentException {
         return switch (value) {
-            case 1 -> Semester.SEMESTER_1;
-            case 2 -> Semester.SEMESTER_2;
-            case 3 -> Semester.SPECIAL_TERM_1;
-            case 4 -> Semester.SPECIAL_TERM_2;
-            default -> throw new IllegalArgumentException(
-                    "Unknown semester for value '" + value + "'");
+        case 1 -> Semester.SEMESTER_1;
+        case 2 -> Semester.SEMESTER_2;
+        case 3 -> Semester.SPECIAL_TERM_1;
+        case 4 -> Semester.SPECIAL_TERM_2;
+        default -> throw new IllegalArgumentException(
+                "Unknown semester for value '" + value + "'");
         };
     }
 
