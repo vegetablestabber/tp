b1905cd2c703a87f132d55bfcdc341ca241e3920
Harii <souper-maario-gaalaxi-wahoo@protonmail.com>
2025-03-21 22:55:11 +0800

    Add URI generation methods and documentation for module list and module information in NUSModsUtil

--- src/main/java/modmate/download/nusmods/NUSModsUtil.java
+++ src/main/java/modmate/download/nusmods/NUSModsUtil.java
@@ -7,10 +7,36 @@ import java.time.Month;
 
 public class NUSModsUtil {
 
+    /**
+     * The file path template for the condensed NUSMods JSON files.
+     * The placeholders %d-%d in the file path will be replaced with the academic year range.
+     * For example, if the academic year is 2023-2024, the file path will be:
+     * "src/main/java/modmate/download/nusmods/nusmods_condensed_2023-2024.json".
+     */
     private static final String CONDENSED_MODS_FILE_PATH = "src/main/java/modmate/download/nusmods/nusmods_condensed_%d-%d.json";
+
+    /**
+     * The base URI for retrieving the list of modules from NUSMods API.
+     * The URI is formatted with two integer values representing the academic year.
+     * For example, to get the module list for the academic year 2021/2022, the URI would be:
+     * https://api.nusmods.com/v2/2021-2022/moduleList.json
+     */
     private static final String MODULE_LIST_URI = "https://api.nusmods.com/v2/%d-%d/moduleList.json";
+
+    /**
+     * The base URI for accessing module information from the NUSMods API.
+     * The URI is formatted with the academic year and module code.
+     * Example usage: String.format(MODULE_URI, 2021, 2022, "CS2113");
+     */
     private static final String MODULE_URI = "https://api.nusmods.com/v2/%d-%d/modules/%s.json";
 
+    /**
+     * Generates a URI for the module list for a given academic year.
+     *
+     * @param startYear The starting year of the academic year (must be greater than 2020).
+     * @return A URI pointing to the module list for the specified academic year.
+     * @throws URISyntaxException If the generated URI string is not a valid URI.
+     */
     public static URI getUriForModuleList(int startYear) throws URISyntaxException {
         assert startYear > 2020;
 
@@ -20,6 +46,14 @@ public class NUSModsUtil {
         return new URI(uriString);
     }
 
+    /**
+     * Generates a URI for the specified module code and academic year.
+     *
+     * @param moduleCode The code of the module (e.g., "CS2113").
+     * @param startYear The starting year of the academic year (e.g., 2023 for the academic year 2023/2024).
+     * @return A URI object representing the module's URL.
+     * @throws URISyntaxException If the generated URI string is not a valid URI.
+     */
     public static URI getUriForModule(String moduleCode, int startYear) throws URISyntaxException {
         assert startYear > 2020;
 
